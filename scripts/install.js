4516826a4ccb061a2f78f7dc54f22ff9cacaa296
microshine <microshine@mail.ru>
2016-09-25 01:23:10 +0300

    Fix scripts

--- scripts/install.js
+++ scripts/install.js
@@ -1,23 +1,13 @@
 /// <reference path="../typings/index.d.ts" />
+var concat = require("./concat") 
 
-var fs = require("fs");
-var os = require("os");
-var FILE_ENCODING = "utf-8";
-
-function concat(files, distPath) {
-    var out = files.map(function (file) {
-        if (!fs.existsSync(file))
-            throw new Error("File '" + file + "' doesn't exist");
-        return fs.readFileSync(file, FILE_ENCODING);
-    });
-    fs.writeFileSync(distPath, out.join(os.EOL), FILE_ENCODING);
-    console.log(distPath + " built.");
-}
-
+// Concat min files
 concat([
    "node_modules/webcrypto-core/build/webcrypto-core.min.js", 
    "build/webcrypto-liner.min.js" 
 ], "webcrypto-liner.min.js");
+
+// Concat full files
 concat([
    "node_modules/webcrypto-core/build/webcrypto-core.js", 
    "build/webcrypto-liner.js" 
