1f60ab28c3f40d70e4a591deacff0c0773939f19
microshine <microshine@mail.ru>
2016-09-25 22:22:16 +0300

    Fix error with native crypto for IE - elliptic.js creats it own crypto object. So we must check for msCrypto first

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,6 @@
 namespace webcrypto.liner {
     let _w: any = window;
 
-    export const nativeCrypto: NativeCrypto = _w.crypto || _w.msCrypto;
+    export const nativeCrypto: NativeCrypto = _w.msCrypto || _w.crypto;
     export const nativeSubtle = nativeCrypto.subtle || (nativeCrypto as any).webkitSubtle;
 }
