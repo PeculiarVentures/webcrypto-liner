da40b40c00c343f226ef234b47eb507eb4675fe9
microshine <microshine@mail.ru>
2019-03-22 16:59:19 +0300

    Remove comment

--- src/key.ts
+++ src/key.ts
@@ -12,27 +12,3 @@ export class CryptoKey extends core.CryptoKey {
     this.algorithm = { ...algorithm };
   }
 }
-
-// export class CryptoKey implements NativeCryptoKey {
-//     public key: any;
-//     public algorithm: KeyAlgorithm;
-//     public extractable: boolean;
-//     public type: KeyType;
-//     public usages: KeyUsage[];
-
-//     constructor(options: ICryptoKeyOptions) {
-//         this.algorithm = options.algorithm;
-//         if (options.type) {
-//             this.type = options.type;
-//         }
-//         this.extractable = options.extractable;
-//         this.usages = options.usages;
-//     }
-
-//     public copy(usages: KeyUsage[]) {
-//         const { algorithm, type, extractable } = this;
-//         const key = new CryptoKey({ algorithm, type, extractable, usages });
-//         key.key = this.key;
-//         return key;
-//     }
-// }
